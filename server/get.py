21ea7ba8bf91288cfe20214da1b69a8dd0075540
riiiiiiiiin <135827690+riiiiiiiiin@users.noreply.github.com>
2023-09-21 15:10:30 +0900

    Add files via upload

--- server/get.py
+++ server/get.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # サーバーのURL
 url = 'http://localhost:3000/matches/10'
@@ -10,4 +11,4 @@ params = {'token': '1234'}
 response = requests.get(url, params=params)
 
 # サーバーからの応答を表示
-print("レスポンス内容:", response)
+print("レスポンス内容:", response.json()["id"])
