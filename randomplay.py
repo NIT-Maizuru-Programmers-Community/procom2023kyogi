e0715a3e73b24881996c26ed83d3c46f3c082ef5
s9122 <s9122@g.maizuru-ct.ac.jp>
2023-10-13 14:49:42 +0900

    軽微な修正

--- randomplay.py
+++ randomplay.py
@@ -12,17 +12,17 @@ def randomplay(field,x,y):
             if field[x+move[j-8][0]][y+move[j-8][1]].CanPlace(1):
                 for _ in range(3):
                     p.append(j)
-                if field[x][y].structure == 2:
-                    print ("l")
-                    for _ in range(5):
-                        p.append(j)
+                flag = field[x][y].GetStructureColor()
+                if flag == "gray":
+                    p = [j]
+                    return p
 
         except:
             continue
     for k in range(12,16):
         try:
             if field[x+move[k-12][0]][y+move[k-12][1]].CanBreak(1):
-                for _ in range(6):
+                for _ in range(5):
                     p.append(k)
         except:
             continue
