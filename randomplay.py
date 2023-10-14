cc75db21d8fe5530b46b57e0ba722fa83d2bd2c1
s9122 <s9122@g.maizuru-ct.ac.jp>
2023-10-15 03:41:37 +0900

    修正

--- randomplay.py
+++ randomplay.py
@@ -31,7 +31,7 @@ def CanEnter(field):
     return ((field.structure != 1) and (field.wall != 2) and (field.mason == 0))
 
 def CanPlace(field):
-    if ((field.structure != 2) and (field.wall == 0) and (field.mason == 0)):
+    if ((field.structure != 2) and (field.wall != 1) and (field.mason == 0)):
         return True
     else:
         return False
@@ -59,25 +59,28 @@ def randomplay(field,x,y,size):
     for i in range(4,16):
         try:
             if 3 < i < 8:
-                if (CanEnter(field[x+move[i][0]][y+move[i][1]])):# and IsOutOfSize(x+move[i][0],y+move[i][1],size):
+                if (CanEnter(field[x+move[i][0]][y+move[i][1]])) and (IsOutOfSize(x+move[i][0],y+move[i][1],size)==False):
                     p.append(i)
             if 7 < i < 12:
-                if (CanPlace(field[x+move[i-8][0]][y+move[i-8][1]])):# and IsOutOfSize(x+move[i-8][0],y+move[i-8][1],size):
+                print(field[x+move[i-8][0]][y+move[i-8][1]].wall,x+move[i-8][0],y+move[i-8][1])
+                if (CanPlace(field[x+move[i-8][0]][y+move[i-8][1]])) and (IsOutOfSize(x+move[i-8][0],y+move[i-8][1],size)==False):
                     for _ in range(3):
                         p.append(i)
             if 11 < i < 16:
-                if (CanBreak(field[x+move[i-12][0]][y+move[i-12][1]])):# and IsOutOfSize(x+move[i-12][0],y+move[i-12][1],size):
+                if (CanBreak(field[x+move[i-12][0]][y+move[i-12][1]])) and (IsOutOfSize(x+move[i-12][0],y+move[i-12][1],size)==False):
                     return [i]
         except:
             continue
     print(p)
     a = random.choice(p)
-    print(a)
     type = TypeJudge(a)
-    if 3 < i < 8:
+    print(a)
+    if 3 < a < 8:
         dir = vec2dir(move[a][0],move[a][1])
+        print(type,move[a][0],move[a][1])
     elif 7 < a < 12:
         dir = vec2dir(move[a-8][0],move[a-8][1])
+        print(type,move[a-8][0],move[a-8][1])
     elif 11 < a < 16:
         dir = vec2dir(move[a-12][0],move[a-12][1])
     return type, dir
