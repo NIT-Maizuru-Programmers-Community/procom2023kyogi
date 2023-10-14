164a85bfc295a48e229928f3a632424686f6eb89
Tomatomato <tomatomato2048@gmail.com>
2023-10-15 01:31:38 +0900

    暫定これプルして

--- solverSimple.py
+++ solverSimple.py
@@ -150,6 +150,9 @@ def Process():
 
 # グラフ描画処理
 def ShowCells():
+    global CurrentTurn
+    global Response
+
     while True:
         Board = Response["board"]
         for x in range(0, Size):
@@ -190,6 +193,6 @@ def ShowCells():
         plt.pause(0.1)
 
 
-Thread = threading.Thread(target=Process)
-Thread.start()
+Thread1 = threading.Thread(target=Process)
+Thread1.start()
 ShowCells()
